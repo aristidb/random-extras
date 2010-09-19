f0b38846a3fb2f62fda7e827fedc4ef329e5a4eb
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-09-19 16:42:44 +0200

    generalise a bit

--- Control/Monad/Random/Extras.hs
+++ Control/Monad/Random/Extras.hs
@@ -15,37 +15,53 @@ module Control.Monad.Random.Extras
   -- * Random functions
   -- ** Shuffling
   shuffle
-, shuffle'
+, shuffleSeq
   -- ** Choice
+, choiceExtract
+, choiceExtractSeq
 , choice
 )
 where
 
-import Control.Monad (liftM)
-import Control.Monad.Loops (unfoldrM)
+import Control.Monad (MonadPlus, liftM)
+import Control.Monad.Loops (unfoldrM')
 import Control.Monad.Random (MonadRandom, getRandomR)
 import qualified Data.Sequence as Seq
 import Data.Sequence ((><), ViewL((:<)))
 
-extract :: Seq.Seq a -> Int -> Maybe (a, Seq.Seq a)
-extract s i | Seq.null r = Nothing
-            | otherwise  = Just (b, a >< c)
-  where (a, r) = Seq.splitAt i s 
-        (b :< c) = Seq.viewl r
+extract :: [a] -> Int -> Maybe (a, [a])
+extract s i | null r    = Nothing
+            | otherwise = Just (b, a ++ c)
+    where (a, r) = splitAt i s
+          (b : c) = r
+
+extractSeq :: Seq.Seq a -> Int -> Maybe (a, Seq.Seq a)
+extractSeq s i | Seq.null r = Nothing
+               | otherwise  = Just (b, a >< c)
+    where (a, r) = Seq.splitAt i s 
+          (b :< c) = Seq.viewl r
 
 -- | Shuffle a list randomly. The method is based on Oleg Kiselyov's 
 -- /perfect shuffle/ <http://okmij.org/ftp/Haskell/perfect-shuffle.txt>,
 -- but much simpler because it uses existing data structures. The efficiency
 -- of both methods should be comparable.
 shuffle :: (MonadRandom m) => [a] -> m [a]
-shuffle = shuffle' . Seq.fromList
+shuffle = shuffleSeq . Seq.fromList
 
--- | Shuffle a Data.Sequence randomly. This is being used by 'shuffle',
+-- | Shuffle a sequence randomly. This is being used by 'shuffle',
 -- so it logically uses the same method.
-shuffle' :: (MonadRandom m) => Seq.Seq a -> m [a]
-shuffle' = unfoldrM step
-    where step s = extract s `liftM` getRandomR range
-              where range = (0, max (Seq.length s - 1) 0)
+shuffleSeq :: (MonadRandom m, MonadPlus f) => Seq.Seq a -> m (f a)
+shuffleSeq = unfoldrM' choiceExtractSeq
+
+-- | Randomly choose and extract an element from a list.
+choiceExtract :: (MonadRandom m) => [a] -> m (Maybe (a, [a]))
+choiceExtract [] = return Nothing
+choiceExtract xs = extract xs `liftM` getRandomR (0, length xs - 1)
+                    
+-- | Randomly choose and extract an element from a sequence.
+choiceExtractSeq :: (MonadRandom m) => Seq.Seq a -> m (Maybe (a, Seq.Seq a))
+choiceExtractSeq s | Seq.null s = return Nothing
+                   | otherwise  = extractSeq s `liftM` getRandomR (0, Seq.length s - 1)
 
 -- | Select a random element from a list.
 -- Complexity: O(n).

c26b0f46eb948e5abe52cd2ebc11e9f5b97c435f
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-09-19 19:31:52 +0200

    vastly improve efficiency of shuffling and sampling

--- Control/Monad/Random/Extras.hs
+++ Control/Monad/Random/Extras.hs
@@ -30,18 +30,23 @@ where
 import Control.Monad (MonadPlus, liftM)
 import Control.Monad.Loops (unfoldrM')
 import Control.Monad.Random (MonadRandom, getRandomR)
+import Data.List (mapAccumL)
+import Data.Maybe (fromJust)
 import qualified Data.Sequence as Seq
 import Data.Sequence ((><), ViewL((:<)))
 
-extract :: [a] -> Int -> Maybe (a, [a])
+(.:) :: (c -> c') -> (a -> b -> c) -> (a -> b -> c')
+(.:) = (.).(.)
+
+extract :: [a] -> Int -> Maybe ([a], a)
 extract s i | null r    = Nothing
-            | otherwise = Just (b, a ++ c)
+            | otherwise = Just (a ++ c, b)
     where (a, r) = splitAt i s
           (b : c) = r
 
-extractSeq :: Seq.Seq a -> Int -> Maybe (a, Seq.Seq a)
+extractSeq :: Seq.Seq a -> Int -> Maybe (Seq.Seq a, a)
 extractSeq s i | Seq.null r = Nothing
-               | otherwise  = Just (b, a >< c)
+               | otherwise  = Just (a >< c, b)
     where (a, r) = Seq.splitAt i s 
           (b :< c) = Seq.viewl r
           
@@ -60,32 +65,40 @@ shuffle = shuffleSeq . Seq.fromList
 -- so it logically uses the same method.
 --
 -- Complexity: O(n * log n)
-shuffleSeq :: (MonadRandom m, MonadPlus f) => Seq.Seq a -> m (f a)
-shuffleSeq = unfoldrM' choiceExtractSeq
+shuffleSeq :: (MonadRandom m) => Seq.Seq a -> m [a]
+shuffleSeq s = do
+  let len = Seq.length s
+  samples <- mapM (\n -> getRandomR (0, n)) [len - 1, len - 2 .. 0]
+  return (shuffleSeq' s samples)
+  
+shuffleSeq' = snd .: mapAccumL (fromJust .: extractSeq)
 
 -- Sampling
 
 -- | Take a random sample from a list.
 sample :: (MonadRandom m) => Int -> [a] -> m [a]
-sample n xs = take n `liftM` shuffle xs
+sample n = sampleSeq n . Seq.fromList
 
 -- | Take a random sample from a sequence.
 sampleSeq :: (MonadRandom m) => Int -> Seq.Seq a -> m [a]
-sampleSeq n s = take n `liftM` shuffleSeq s
+sampleSeq n s = do
+  let len = Seq.length s
+  samples <- mapM (\n -> getRandomR (0, n)) (take n [len - 1, len - 2 .. 0])
+  return (shuffleSeq' s samples)
 
 -- Choice
 
 -- | Randomly choose and extract an element from a list.
 --
 -- Complexity: O(n)
-choiceExtract :: (MonadRandom m) => [a] -> m (Maybe (a, [a]))
+choiceExtract :: (MonadRandom m) => [a] -> m (Maybe ([a], a))
 choiceExtract [] = return Nothing
 choiceExtract xs = extract xs `liftM` getRandomR (0, length xs - 1)
                     
 -- | Randomly choose and extract an element from a sequence.
 --
 -- Complexity: O(log n)
-choiceExtractSeq :: (MonadRandom m) => Seq.Seq a -> m (Maybe (a, Seq.Seq a))
+choiceExtractSeq :: (MonadRandom m) => Seq.Seq a -> m (Maybe (Seq.Seq a, a))
 choiceExtractSeq s | Seq.null s = return Nothing
                    | otherwise  = extractSeq s `liftM` getRandomR (0, Seq.length s - 1)
 

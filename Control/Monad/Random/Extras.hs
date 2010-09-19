fdd2266c254bdc48eaa6fe8c915a9c5d3167e2ff
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-09-19 16:50:49 +0200

    add choiceSeq and more documentation

--- Control/Monad/Random/Extras.hs
+++ Control/Monad/Random/Extras.hs
@@ -20,6 +20,7 @@ module Control.Monad.Random.Extras
 , choiceExtract
 , choiceExtractSeq
 , choice
+, choiceSeq
 )
 where
 
@@ -45,26 +46,42 @@ extractSeq s i | Seq.null r = Nothing
 -- /perfect shuffle/ <http://okmij.org/ftp/Haskell/perfect-shuffle.txt>,
 -- but much simpler because it uses existing data structures. The efficiency
 -- of both methods should be comparable.
+--
+-- Complexity: O(n * log n)
 shuffle :: (MonadRandom m) => [a] -> m [a]
 shuffle = shuffleSeq . Seq.fromList
 
 -- | Shuffle a sequence randomly. This is being used by 'shuffle',
 -- so it logically uses the same method.
+--
+-- Complexity: O(n * log n)
 shuffleSeq :: (MonadRandom m, MonadPlus f) => Seq.Seq a -> m (f a)
 shuffleSeq = unfoldrM' choiceExtractSeq
 
 -- | Randomly choose and extract an element from a list.
+--
+-- Complexity: O(n)
 choiceExtract :: (MonadRandom m) => [a] -> m (Maybe (a, [a]))
 choiceExtract [] = return Nothing
 choiceExtract xs = extract xs `liftM` getRandomR (0, length xs - 1)
                     
 -- | Randomly choose and extract an element from a sequence.
+--
+-- Complexity: O(log n)
 choiceExtractSeq :: (MonadRandom m) => Seq.Seq a -> m (Maybe (a, Seq.Seq a))
 choiceExtractSeq s | Seq.null s = return Nothing
                    | otherwise  = extractSeq s `liftM` getRandomR (0, Seq.length s - 1)
 
 -- | Select a random element from a list.
+--
 -- Complexity: O(n).
 choice :: (MonadRandom m) => [a] -> m a
 choice [] = error "Control.Monad.Random.Extras.choice: empty list"
 choice xs = (xs !!) `liftM` getRandomR (0, length xs - 1)
+
+-- | Select a random element from a sequence.
+--
+-- Complexity: O(log n).
+choiceSeq :: (MonadRandom m) => Seq.Seq a -> m a
+choiceSeq s | Seq.null s = error "Control.Monad.Random.Extras.choiceSeq: empty sequence"
+            | otherwise  = Seq.index s `liftM` getRandomR (0, Seq.length s - 1)

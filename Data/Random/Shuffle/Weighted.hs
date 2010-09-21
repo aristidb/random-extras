4e2721bfdff87b599791d44cbed03cb237360be1
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-09-21 18:45:58 +0200

    use exclusive uniform distribution to unskew integral weighted shuffles

--- Data/Random/Shuffle/Weighted.hs
+++ Data/Random/Shuffle/Weighted.hs
@@ -50,35 +50,36 @@ import Control.Applicative ((<$>))
 import Data.Random.RVar
 import Data.Random.Distribution
 import Data.Random.Distribution.Uniform
+import Data.Random.Distribution.Uniform.Exclusive
 import qualified Data.Map as M
 
 moduleError :: String -> String -> a
 moduleError n s = error $ "Data.Random.Shuffle.Weighted." ++ n ++ ": " ++ s
 
 -- | Randomly shuffle a CDF map according to its weights.
-weightedShuffleCDF :: (Num w, Ord w, Distribution Uniform w) => M.Map w a -> RVar [a]
+weightedShuffleCDF :: (Num w, Ord w, Distribution Uniform w, Excludable w) => M.Map w a -> RVar [a]
 weightedShuffleCDF m | M.null m  = return []
                      | otherwise = weightedChoiceExtractCDF m >>= \(m', a) -> (a:) <$> weightedShuffleCDF m'
 
 -- | Randomly shuffle a weighted list according to its weights.
-weightedShuffle :: (Num w, Ord w, Distribution Uniform w) => [(w, a)] -> RVar [a]
+weightedShuffle :: (Num w, Ord w, Distribution Uniform w, Excludable w) => [(w, a)] -> RVar [a]
 weightedShuffle = weightedShuffleCDF . cdfMapFromList
 
 -- | Randomly draw /n/ elements from a CDF map according to its weights.
-weightedSampleCDF :: (Num w, Ord w, Distribution Uniform w) => Int -> M.Map w a -> RVar [a]
+weightedSampleCDF :: (Num w, Ord w, Distribution Uniform w, Excludable w) => Int -> M.Map w a -> RVar [a]
 weightedSampleCDF n m | M.null m || n <= 0 = return []
                       | otherwise          = weightedChoiceExtractCDF m >>= \(m', a) -> (a:) <$> weightedSampleCDF (n - 1) m'
 
 -- | Randomly draw /n/ elements from a weighted list according to its weights.
-weightedSample :: (Num w, Ord w, Distribution Uniform w) => Int -> [(w, a)] -> RVar [a]
+weightedSample :: (Num w, Ord w, Distribution Uniform w, Excludable w) => Int -> [(w, a)] -> RVar [a]
 weightedSample n = weightedSampleCDF n . cdfMapFromList
 
 -- | Randomly extract an element from a CDF map according to its weights. The
 -- element is removed and the resulting "weight gap" closed.
-weightedChoiceExtractCDF :: (Num w, Ord w, Distribution Uniform w) => M.Map w a -> RVar (M.Map w a, a)
+weightedChoiceExtractCDF :: (Num w, Ord w, Distribution Uniform w, Excludable w) => M.Map w a -> RVar (M.Map w a, a)
 weightedChoiceExtractCDF m | M.null m         = moduleError "weightedChoiceExtractCDF" "empty map"
                            | M.null exceptMax = return (exceptMax, maxE)
-                           | otherwise        = extract <$> uniform 0 wmax
+                           | otherwise        = extract <$> uniformExclusive 0 wmax
     where Just ((wmax, maxE), exceptMax) = M.maxViewWithKey m
           extract w = (a `M.union` M.mapKeysMonotonic (subtract gap) c, b)
               where (a, e, r') = M.splitLookup w m

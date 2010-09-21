9d2f4e5ecc0cdd3b9405d6855bb9b50f6e0c2a71
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-09-21 13:18:46 +0200

    add export list and refactor list->cdf map generation into function

--- Data/Random/Shuffle/Weighted.hs
+++ Data/Random/Shuffle/Weighted.hs
@@ -11,6 +11,14 @@ Functions for shuffling elements according to weights.
 -}
 
 module Data.Random.Shuffle.Weighted
+(
+  cdfMapFromList
+, weightedChoiceExtractCDF
+, weightedShuffleCDF
+, weightedShuffle
+, weightedSampleCDF
+, weightedSample
+)
 where
   
 import Control.Applicative ((<$>))
@@ -21,6 +29,10 @@ import qualified Data.Map as M
 
 moduleError :: String -> String -> a
 moduleError n s = error $ "Data.Random.Shuffle.Weighted." ++ n ++ ": " ++ s
+
+-- | Generate a CDF map from a list of 
+cdfMapFromList :: Num w => [(w, a)] -> M.Map w a
+cdfMapFromList = M.fromAscList . scanl1 (\(w1, _) (w2, x) -> (w1 + w2, x))
   
 weightedChoiceExtractCDF :: (Num w, Ord w, Distribution Uniform w) => M.Map w a -> RVar (M.Map w a, a)
 weightedChoiceExtractCDF m | M.null m  = moduleError "weightedChoiceExtractCDF" "empty map"
@@ -38,11 +50,11 @@ weightedShuffleCDF m | M.null m  = return []
                      | otherwise = weightedChoiceExtractCDF m >>= \(m', a) -> (a:) <$> weightedShuffleCDF m'
 
 weightedShuffle :: (Num w, Ord w, Distribution Uniform w) => [(w, a)] -> RVar [a]
-weightedShuffle = weightedShuffleCDF . M.fromAscList . scanl1 (\(w1, _) (w2, x) -> (w1 + w2, x))
+weightedShuffle = weightedShuffleCDF . cdfMapFromList
 
 weightedSampleCDF :: (Num w, Ord w, Distribution Uniform w) => Int -> M.Map w a -> RVar [a]
 weightedSampleCDF n m | M.null m || n <= 0 = return []
                       | otherwise          = weightedChoiceExtractCDF m >>= \(m', a) -> (a:) <$> weightedSampleCDF (n - 1) m'
 
 weightedSample :: (Num w, Ord w, Distribution Uniform w) => Int -> [(w, a)] -> RVar [a]
-weightedSample n = weightedSampleCDF n . M.fromAscList . scanl1 (\(w1, _) (w2, x) -> (w1 + w2, x))
+weightedSample n = weightedSampleCDF n . cdfMapFromList
